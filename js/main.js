467c3073db6e69dfe1cccea5104c1c14dfabacc4
MasamiGit <info@masami.main.jp>
2018-08-19 04:36:14 +0900

    commt

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-// (function () {
-//     'use strict';
-//     var home = document.getElementById('home');
-//     home.addEventListener('click', function () {
-//             this.className = 'pushed';
-//             // var home = document.getElementById('home');
-//             // home.addEventListener()
+(function () {
+    'use strict';
+    var home = document.getElementById('home');
+    home.addEventListener('click', function () {
+            this.className = 'pushed';
+            // var home = document.getElementById('home');
+            // home.addEventListener()
         
-//     });
-// }());
+    });
+}());
