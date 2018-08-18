35d23bf5f1001994c467f4c53974e0afb0ea1c50
MasamiGit <info@masami.main.jp>
2018-08-19 03:43:18 +0900

    '819commit';

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// (function () {
+//     'use strict';
+//     var home = document.getElementById('home');
+//     home.addEventListener('click', function () {
+//             this.className = 'pushed';
+//             // var home = document.getElementById('home');
+//             // home.addEventListener()
+        
+//     });
+// }());
